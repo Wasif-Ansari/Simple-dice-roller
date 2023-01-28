2da8b33d8c34669802077a8e11f9b20928b7a832
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-28 17:37:38 +0530

    making function to roll using random module

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ Dice = {
     6 : '⚅'
 }
 
+def roll():
+    from random import randint
+    dice_choice = randint(1, 6)
+
+    dice_lbl = Label(App, text=Dice[dice_choice], font=('Times', 100), width=2)
+    dice_lbl.grid(row=0, column=0, padx=40)
+
+
 # First dice character to show when the app starts
 lbl = Label(App, text=Dice[0], font=('Times', 100))
 lbl.grid(row=0, column=0, padx=40)
