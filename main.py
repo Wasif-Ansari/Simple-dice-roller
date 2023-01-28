27676e88360124b3eaf64a8ca1e46806f39b8602
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-28 17:28:34 +0530

    getting dice characters

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ from tkinter import *
 App = Tk()
 App.title("Dice Roller")
 
+Dice = {
+    0 : '🎲',
+    1 : '⚀',
+    2 : '⚁',
+    3 : '⚂',
+    4 : '⚃',
+    5 : '⚄',
+    6 : '⚅'
+}
+
 App.mainloop()
